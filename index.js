4a4ffe2d3873fc9968bb19386c82b27b64e8c3bf
KH Rasedul Islam <87899184+khrasedul-dev@users.noreply.github.com>
2022-10-06 02:11:18 +0600

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const {Telegraf} = require('telegraf')
+const express = require('express')
+
+const app = express()
+
+const bot = new Telegraf(process.env.TOKEN)
+
+
+bot.start(ctx=>{
+    ctx.reply("Bot started")
+})
+
+bot.command('test',ctx=>{
+    ctx.reply("This is test command")
+})
+
+app.use(bot.webhookCallback(`/${process.env.TOKEN}`))
+bot.telegram.setWebhook(`${process.env.DOMAIN}:8443/${process.env.TOKEN}`)
+
+app.get("/", (req, res) => {
+    res.json({"status": "the bot is running"})
+})
+
+app.listen(5000, ()=>{
+    console.log("Bot is running")
+})
